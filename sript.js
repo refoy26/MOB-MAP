5b4ef5b956dfc94973bb07d9e8f89991ba11b5b4
refoy26 <racefoy@gmail.com>
2026-06-11 10:56:46 -0400

    Update sript.js

--- sript.js
+++ sript.js
@@ -1,1 +1,34 @@
+// Create map centered on Norfolk
+const map = L.map('map').setView([36.8508, -76.2859], 13);
 
+// Add map tiles (like Google Maps style)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Add markers
+properties.forEach(property => {
+
+    const marker = L.marker([property.lat, property.lng]).addTo(map);
+
+    const popupContent = `
+        <div>
+            <div class="popup-title">${property.name}</div>
+            <div>${property.address}</div>
+            <div><strong>Price:</strong> ${property.price}</div>
+            <div><strong>Size:</strong> ${property.sqft}</div>
+        </div>
+    `;
+
+    marker.bindPopup(popupContent);
+
+    // Hover effect
+    marker.on('mouseover', function () {
+        this.openPopup();
+    });
+
+    marker.on('mouseout', function () {
+        this.closePopup();
+    });
+});
+``
