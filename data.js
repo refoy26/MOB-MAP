666454df3d9de120f4602667166638f11ed0807b
refoy26 <racefoy@gmail.com>
2026-06-11 11:22:30 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,18 +1,10 @@
 const properties = [
     {
-        name: "Medical Office Building",
-        address: "123 Main St, Norfolk, VA",
-        price: "$2,000,000",
-        sqft: "10,000 SF",
+        name: "Test Property 1",
+        address: "Norfolk, VA",
+        price: "$1,000,000",
+        sqft: "5,000 SF",
         lat: 36.8508,
         lng: -76.2859
-    },
-    {
-        name: "Retail Plaza",
-        address: "456 Granby St, Norfolk, VA",
-        price: "$3,500,000",
-        sqft: "20,000 SF",
-        lat: 36.8529,
-        lng: -76.2930
     }
 ];
